2d8f579848059944f9608e79ea4efd44f2f01e5a
Levin Schmid <levin.schmid@mein.gmx>
2024-10-24 11:37:31 +0200

    .

--- Insert_Asisstent.sql
+++ Insert_Asisstent.sql
@@ -1,1 +1,22 @@
-Insert INTO Assistent(Vorname, Nachname, Geburtsdatum) VALUES('Max', 'Mustermann', '1990-01-01')
+Insert INTO Assistent(Vorname, Nachname, Geburtsdatum, PersonalNr_Chef)
+VALUES('Lionel', 'Messi', '1987-06-24', 1),
+    ('Xherdan', 'Shaqiri', '1991-10-10', 1),
+    ('Khedira', 'Sami', '1987-04-04', 1),
+    ('Cristiano', 'Ronaldo', '1985-02-05', 1),
+    ('Wayne', 'Rooney', '1985-10-24', 1),
+    ('Arjen', 'Robben', '1984-01-23', 1),
+    ('Andrea', 'Pirlo', '1979-05-19', 1),
+    ('Robert', 'Lewandowski', '1988-08-21', 1),
+    ('Luis', 'Suàrez', '1987-01-24', 1),
+    ('Alexis', 'Sanchez', '1988-12-19', 1);
+/*Messi	M	1987-06-24	
+Shaqiri	Xherdan	1991-10-10	Tesla Nikola
+Khedira	Sami	1987-04-04	Von Matt Peter
+Ronaldo	Cristiano	1985-02-05	Von Matt Peter
+Rooney	Wayne	1985-10-24	
+Robben	Arjen	1984-01-23	van 't Hoff Jacobus
+Pirlo	Andrea	1979-05-19	Einstein Albert
+Lewandowski	Robert	1988-08-21	Von Matt Peter
+Suàrez	Luis	1987-01-24	Röntgen Wilhelm Conrad
+Sanchez	Alexis	1988-12-19	Curie Marie
+*/
