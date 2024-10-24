838e0c6be64aa7e93f638781bbdb5ca28296b6cd
Levin Schmid <levin.schmid@mein.gmx>
2024-10-24 14:21:26 +0200

    .

--- insertVorlesungen.sql
+++ insertVorlesungen.sql
@@ -2,28 +2,41 @@
 
 INSERT INTO Vorlesungen (Titel, MatrikalNr, PersonalNr)
 VALUES
-    ('Physikalische Chemie', '07-0633-6', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Curie Marie')),
-    ('Kinetik', '09-0665-1', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'van''t Hoff Jacobus')),
-    ('Biochemie', '09-1079-4', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'van''t Hoff Jacobus')),
-    ('Elektrotechnik', '09-4845-0', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Tesla Nikola')),
-    ('Radiologie', '09-4845-0', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Röntgen Wilhelm Conrad')),
-    ('Biochemie', '09-7270-8', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'van''t Hoff Jacobus')),
-    ('Literaturgeschichte', '09-9370-0', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Von Matt Peter')),
-    ('Kinetikö', '10-3201-8', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'van''t Hoff Jacobus')),
-    ('Literaturgeschichte', '08-5694-8', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Von Matt Peter')),
-    ('Elektrotechnik', '12-5776-4', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Tesla Nikola')),
-    ('Ethik', '12-8867-9', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Matt Peter')),
-    ('Ethik', '13-3704-2', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Matt Peter')),
-    ('Literaturgeschichte', '13-3704-2', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Von Matt Peter')),
-    ('Sprachtheorie', '13-3704-2', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Von Matt Peter')),
-    ('Sprachtheorie', '13-3963-7', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Von Matt Peter')),
-    ('Ethik', '13-3963-7', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Matt Peter')),
-    ('Literaturgeschichte', '13-3963-7', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Von Matt Peter')),
-    ('Kinetik', '13-4372-1', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'van''t Hoff Jacobus')),
-    ('Elektrotechnik', '13-5660-2', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Tesla Nikola')),
-    ('Allgemeine Relativitätstheorie', '13-5660-2', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Röntgen Wilhelm Conrad')),
-    ('Kinetik', '13-5660-2', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'van''t Hoff Jacobus')),
-    ('Allgemeine Relativitätstheorie', '14-0556-8', (SELECT top 1 PersonalNr FROM Professor WHERE Concat(Nachname, ' ', Vorname) = 'Röntgen Wilhelm Conrad'));
+    ('Physikalische Chemie', '07-0633-6', 2),
+    ('Kinetik', '09-0665-1', 7),
+    ('Biochemie', '09-1079-4', 7),
+    ('Elektrotechnik', '09-4845-0', 4),
+    ('Radiologie', '09-4845-0', 5),
+    ('Biochemie', '09-7270-8', 7),
+    ('Literaturgeschichte', '09-9370-0', 3),
+    ('Kinetik', '10-3201-8', 7),
+    ('Literaturgeschichte', '08-5694-8', 3),
+    ('Elektrotechnik', '12-5776-4', 4),
+    ('Ethik', '12-8867-9', 3),
+    ('Ethik', '13-3704-2', 3),
+    ('Literaturgeschichte', '13-3704-2', 3),
+    ('Sprachtheorie', '13-3704-2', 3),
+    ('Sprachtheorie', '13-3963-7', 3),
+    ('Ethik', '13-3963-7', 3),
+    ('Literaturgeschichte', '13-3963-7', 3),
+    ('Kinetik', '13-4372-1', 7),
+    ('Elektrotechnik', '13-5660-2', 4),
+    ('Allgemeine Relativitätstheorie', '13-5660-2', 1),
+    ('Kinetik', '13-5660-2', 7),
+    ('Allgemeine Relativitätstheorie', '14-0556-8', 1);
 
+/*
+Ethik	Von Matt Peter 3
+Elektrotechnik	Tesla Nikola 4
+Sprachtheorie	Von Matt Peter 3
+Radiologie	Röntgen Wilhelm Conrad 5
+Allgemeine Relativitätstheorie	Einstein Albert 1
+Quantenphysik	Einstein Albert 1
+Kinetik	van 't Hoff Jacobus 7
+Literaturgeschichte	Von Matt Peter 3
+Astrophysik	Einstein Albert 1
+Biochemie	van 't Hoff Jacobus 7
+Physikalische Chemie	Curie Marie 2
+*/
 
 
