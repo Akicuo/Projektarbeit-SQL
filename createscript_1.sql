3273f4f8b2a0e4e3d9795f7fdad2c621e43030e3
Akicuo <user.of.replit@gmail.com>
2024-10-24 10:30:38 +0200

    fg

--- createscript_1.sql
+++ createscript_1.sql
@@ -2,3 +2,21 @@
 CREATE DATABASE UniDB;
 USE UniDB
 -------------------------------------
+
+CREATE TABLE Vorlesungen (
+    Vorlesung_ID INT NOT NULL,
+    Titel varchar(100) NOT NULL,
+    FOREIGN KEY (Matrikelnummer) REFERENCES Student(Matrikelnummer),
+    PRIMARY KEY (Vorlesung_ID)
+);
+
+
+CREATE TABLE Pruefungen (
+    Pruefung_ID INT IDENTITY(1,1) PRIMARY KEY,
+    Pruefungsaufsicht INT NOT NULL,
+    PruefungsTermin DATE not null
+    FOREIGN KEY (Pruefungsaufsicht) REFERENCES Professor(PersonalNummer),
+    PRIMARY KEY (Pruefung_ID)
+);
+
+--------------
