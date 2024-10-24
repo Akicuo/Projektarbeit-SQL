eb6399b8cafe631a867a2868d472dd48c4e60418
Akicuo <user.of.replit@gmail.com>
2024-10-24 13:57:15 +0200

    qwdqwdq

--- createStudent.sql
+++ createStudent.sql
@@ -1,29 +1,27 @@
-instert into Schueler (MatrikalNr, )
-
-/*
-09-4845-0	Burri	Eliane	01.10.2019	3005 Bern
-12-5776-4	Duss	Guido	01.10.2019	4500 Solothurn
-09-7270-8	Zollinger	Gertrud	01.10.2019	3600 Thun
-08-5694-8	Antonelli	Giorgio	01.10.2018	6204 Sempach
-13-3963-7	Sanchez	Miguel	01.10.2019	
-09-6537-7	Stefanovski	Zoran	01.10.2019	3000 Bern
-10-4336-3	Prieto	Luis	01.10.2019	4000 Basel
-13-4372-1	Isler	Martin	01.10.2019	
-09-1079-4	Di Lavello	Paolo	01.10.2019	5400 Baden
-10-5068-5	Meier	Rolf	01.10.2019	5430 Wettingen
-09-9370-0	Maggi	Marco	01.10.2019	8918 Unterlunkhofen
-09-0523-5	Kurmann	Heike	01.10.2019	6280 Hochdorf
-09-9376-6	Krasniqi	Lelzim	01.10.2019	2905 Courtedoux
-14-0556-8	Léchenne	Jean-Paul	01.10.2019	4500 Solothurn
-09-0665-1	Détraz	Roger	01.10.2017	3215 Lurtigen
-11-8456-6	Dubach	Hans	01.10.2018	3000 Bern
-10-3201-8	Keller	Yvonne	01.10.2019	
-12-0948-3	Weber	Priska	01.10.2019	4125 Riehen
-13-3225-2	Dubuis	Heidi	01.10.2019	6213 Knutwil
-13-5660-2	Stojanovic	Slobodan	01.10.2019	4132 Muttenz
-07-0633-6	Zobrist	Bruno	01.10.2018	5242 Birr
-10-1471-6	Babaja	Slobodanka	01.10.2019	4600 Olten
-10-2466-6	Gugler	Roger	01.10.2018	3400 Burgdorf
-13-3704-2	Genkinger	Marian	01.10.2019	
-12-8867-9	Dell'Amore	Michele	01.10.2019	4900 Langenthal
-*/
+INSERT INTO Schueler (MatrikelNr, Name, Vorname, Geburtsdatum, Wohnort)
+VALUES 
+('09-4845-0', 'Burri', 'Eliane', '01.10.2019', '3005 Bern'),
+('12-5776-4', 'Duss', 'Guido', '01.10.2019', '4500 Solothurn'),
+('09-7270-8', 'Zollinger', 'Gertrud', '01.10.2019', '3600 Thun'),
+('08-5694-8', 'Antonelli', 'Giorgio', '01.10.2018', '6204 Sempach'),
+('13-3963-7', 'Sanchez', 'Miguel', '01.10.2019', NULL),
+('09-6537-7', 'Stefanovski', 'Zoran', '01.10.2019', '3000 Bern'),
+('10-4336-3', 'Prieto', 'Luis', '01.10.2019', '4000 Basel'),
+('13-4372-1', 'Isler', 'Martin', '01.10.2019', NULL),
+('09-1079-4', 'Di Lavello', 'Paolo', '01.10.2019', '5400 Baden'),
+('10-5068-5', 'Meier', 'Rolf', '01.10.2019', '5430 Wettingen'),
+('09-9370-0', 'Maggi', 'Marco', '01.10.2019', '8918 Unterlunkhofen'),
+('09-0523-5', 'Kurmann', 'Heike', '01.10.2019', '6280 Hochdorf'),
+('09-9376-6', 'Krasniqi', 'Lelzim', '01.10.2019', '2905 Courtedoux'),
+('14-0556-8', 'Léchenne', 'Jean-Paul', '01.10.2019', '4500 Solothurn'),
+('09-0665-1', 'Détraz', 'Roger', '01.10.2017', '3215 Lurtigen'),
+('11-8456-6', 'Dubach', 'Hans', '01.10.2018', '3000 Bern'),
+('10-3201-8', 'Keller', 'Yvonne', '01.10.2019', NULL),
+('12-0948-3', 'Weber', 'Priska', '01.10.2019', '4125 Riehen'),
+('13-3225-2', 'Dubuis', 'Heidi', '01.10.2019', '6213 Knutwil'),
+('13-5660-2', 'Stojanovic', 'Slobodan', '01.10.2019', '4132 Muttenz'),
+('07-0633-6', 'Zobrist', 'Bruno', '01.10.2018', '5242 Birr'),
+('10-1471-6', 'Babaja', 'Slobodanka', '01.10.2019', '4600 Olten'),
+('10-2466-6', 'Gugler', 'Roger', '01.10.2018', '3400 Burgdorf'),
+('13-3704-2', 'Genkinger', 'Marian', '01.10.2019', NULL),
+('12-8867-9', 'Dell'Amore', 'Michele', '01.10.2019', '4900 Langenthal');
