6ae2902b1fed3f19a84f8fc0671409861b70eca6
Akicuo <user.of.replit@gmail.com>
2024-10-24 10:34:52 +0200

    end

--- createscript_1.sql
+++ createscript_1.sql
@@ -6,7 +6,8 @@ USE UniDB
 CREATE TABLE Vorlesungen (
     Vorlesung_ID INT NOT NULL,
     Titel varchar(100) NOT NULL,
-    FOREIGN KEY (Matrikelnummer) REFERENCES Student(Matrikelnummer),
+    MatrikalNr int IDENTITY not null,
+    FOREIGN KEY (Matrikalnr) REFERENCES Schueler(Matrikalnr),
     PRIMARY KEY (Vorlesung_ID)
 );
 
@@ -15,8 +16,8 @@ CREATE TABLE Pruefungen (
     Pruefung_ID INT IDENTITY(1,1) PRIMARY KEY,
     Pruefungsaufsicht INT NOT NULL,
     PruefungsTermin DATE not null
-    FOREIGN KEY (Pruefungsaufsicht) REFERENCES Professor(PersonalNummer),
-    PRIMARY KEY (Pruefung_ID)
+    FOREIGN KEY (Pruefungsaufsicht) REFERENCES Professor(PersonalNr),
+    
 );
 
 --------------
