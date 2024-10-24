7cd843776536c23d7df1a94aaf11ac2fb0f48547
Levin Schmid <levin.schmid@mein.gmx>
2024-10-24 10:45:20 +0200

    inSessin_Test

--- createscript_1.sql
+++ createscript_1.sql
@@ -23,3 +23,8 @@ ALTER TABLE Vorlesungen
     ADD CONSTRAINT fk_Vorlesung FOREIGN KEY (Vorlesung_ID) REFERENCES Vorlesungen(Vorlesung_ID);
 
 --------------
+CREATE TABLE InSesion_Test(
+    Pruefung_ID int,
+    MatrikalNr INT
+    Primary Key (Pruefung_ID, MatrikalNr)
+)
